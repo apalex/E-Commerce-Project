e08e4596427609288044be29ff70d8a315540942
Alex Pham <67773092+apalex@users.noreply.github.com>
2023-10-28 10:38:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectDrop = document.querySelector('#countries');
+
+    fetch('https://restcountries.com/v3.1/all').then(res => {
+        return res.json();
+    }).then(data => {
+        let countryOpt = "";
+        var sortedCountries = [];
+        for (var i = 0; i < data.length; i++) {
+            sortedCountries[i] = data[i].name.common;
+        }
+        sortedCountries.sort();
+        for (var i = 0; i < sortedCountries.length; i++) {
+            countryOpt += `<option>${sortedCountries[i]}</option>`;
+        }
+        selectDrop.innerHTML += countryOpt;
+    }).catch(err => {
+        console.log(err);
+    })
+
+});
