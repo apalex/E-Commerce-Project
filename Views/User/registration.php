9b3de15c713f82766b9e0dee80329c94df4579cc
Brandon-JP <98441280+Brandon-JP@users.noreply.github.com>
2023-11-06 22:54:19 -0500

    Update registration.php

--- Views/User/registration.php
+++ Views/User/registration.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop all your electronic needs and save big! | ABDGameStore.com</title>
-    <link rel="stylesheet" href="../../styles.css">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -105,12 +105,12 @@
             <h1>Create an Account</h1><br>
             <h5>Enter your details below</h5><br>
             <div class="registration form">
-                <form action="">
-                    <input type="text" name="" id="" placeholder="First Name" required><br><br>
-                    <input type="text" name="" id="" placeholder="Last Name" required><br><br>
-                    <input type="text" name="" id="" placeholder="Email" required><br><br>
-                    <input type="password" name="" id="" placeholder="Password" required><br><br>
-                    <input type="password" name="" id="" placeholder="Confirm Password" required><br><br><br>
+                <form action="?controller=user&action=insert" method="POST">
+                    <input type="text" name="F_NAME" id="" placeholder="First Name" required><br><br>
+                    <input type="text" name="L_NAME" id="" placeholder="Last Name" required><br><br>
+                    <input type="text" name="EMAIL" id="" placeholder="Email" required><br><br>
+                    <input type="password" name="PASSWD" id="" placeholder="Password" required><br><br>
+                    <input type="password" name="C_PASSWD" id="" placeholder="Confirm Password" required><br><br><br>
                     <button type="submit">Create Account</button>
                     <p>Already have an account? <a href="login.html">Login In</a></p>
                 </form>
