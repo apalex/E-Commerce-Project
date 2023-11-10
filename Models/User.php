b07644a1b5dd29e38a7cb208762ad19eb7fd823b
Brandon-JP <98441280+Brandon-JP@users.noreply.github.com>
2023-11-10 00:21:57 -0500

    Update User.php

--- Models/User.php
+++ Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "mysqldatabase.php";
+include "mysqldatabase.php";
 
 class User {
     public $U_ID;
@@ -16,31 +16,31 @@ class User {
     function __construct($id = -1) {
         global $conn;
 
-        $this -> $U_ID = $id;
+        $this -> U_ID = $id;
         if ($id < 0) {
-            $this -> $Role_ID = 0;
-            $this -> $U_Email = "";
-            $this -> $U_Pass = "";
-            $this -> $F_Name = "";
-            $this -> $L_Name = "";
-            $this -> $Phone_Num = 0;
-            $this -> $Created_On = null;
-            $this -> $Modified_On = null;
+            $this -> Role_ID = 0;
+            $this -> U_Email = "";
+            $this -> U_Pass = "";
+            $this -> F_Name = "";
+            $this -> L_Name = "";
+            $this -> Phone_Num = 0;
+            $this -> Created_On = null;
+            $this -> Modified_On = null;
         } else {
             $sql = "SELECT * FROM `User_Info` WHERE `U_ID` = " . $id;
             $result = $conn -> query($sql);
 
             $data = $result -> fetch_assoc();
 
-            $this -> $U_ID = $id;
-            $this -> $Role_ID = $data['Role_ID'];
-            $this -> $U_Email = $data['U_Email'];
-            $this -> $U_Pass = $data['U_Pass'];
-            $this -> $F_Name = $data['F_Name'];
-            $this -> $L_Name = $data['L_Name'];
-            $this -> $Phone_Num = $data['Phone_Num'];
-            $this -> $Created_On = $data['Created_On'];
-            $this -> $Modified_On = $data['Modified_On'];
+            $this -> U_ID = $id;
+            $this -> Role_ID = $data['Role_ID'];
+            $this -> U_Email = $data['U_Email'];
+            $this -> U_Pass = $data['U_Pass'];
+            $this -> F_Name = $data['F_Name'];
+            $this -> L_Name = $data['L_Name'];
+            $this -> Phone_Num = $data['Phone_Num'];
+            $this -> Created_On = $data['Created_On'];
+            $this -> Modified_On = $data['Modified_On'];
         }
     }
 
@@ -71,19 +71,24 @@ class User {
     function updateEmail($id, $U_Email) {
         global $conn;
         
-        $sql = "UPDATE `User_Info` SET `U_Email` = `$U_Email` WHERE `User_Info` . `U_ID` = $id;";
-        $conn = query($sql);
+        $sql = "UPDATE `user_Info` SET `U_Email` = '$U_Email' WHERE `U_ID` = $id;";
+        $conn -> query($sql);
 
-        var_dump($conn -> $error);
+        if($conn -> connect_error) {
+            die("Connection failed: " . $conn -> connect_error);
+        }
     }
 
     function updatePassword($id, $U_Pass) {
         global $conn;
 
-        $sql = "UPDATE `User_Info` SET `U_Pass` = `$U_Pass` WHERE `User_Info` . `U_ID` = $id;";
-        $conn = query($sql);
+        $sql = "UPDATE `User_Info` SET `U_Pass` = '$U_Pass' WHERE`U_ID` = $id;";
+        $conn -> query($sql);
 
-        var_dump($conn -> $error);
+        if($conn -> connect_error) {
+            die("Connection failed: " . $conn -> connect_error);
+        }
+        
     }
 
     function updateEditUser($id, $F_Name, $L_Name, $Phone_Num) {
