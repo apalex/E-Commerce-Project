97b1cd83d348ff87cf1188a4687c5a1e5d0d42a2
Den123123123 <roznerita.denis@gmail.com>
2023-11-29 18:35:29 -0500

    I put the timer for the slider

--- E-Commerce-Project-main/caroussel.js
+++ E-Commerce-Project-main/caroussel.js
@@ -2,6 +2,8 @@ const productContainers = [...document.querySelectorAll('.productContainer')];
 const nxtBtn = [...document.querySelectorAll('.nxtBtn')];
 const preBtn = [...document.querySelectorAll('.preBtn')];
 
+
+
 productContainers.forEach((item, i)=>{
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
@@ -13,3 +15,25 @@ productContainers.forEach((item, i)=>{
         item.scrollLeft -= containerWidth;
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll('.slider img');
+    var currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach(function(slide) {
+            slide.style.display = 'none';
+        });
+        
+        slides[index].style.display = 'block';
+        
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 5000);
+});
+
