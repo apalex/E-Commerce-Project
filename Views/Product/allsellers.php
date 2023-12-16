fa5adb7d0566f092777b2c4959186518e9945ce4
%apalex% <67773092+apalex@users.noreply.github.com>
2023-12-15 19:40:32 -0500

    Test All Sellers

--- Views/Product/allsellers.php
+++ Views/Product/allsellers.php
@@ -1,1 +1,97 @@
-<!-- NEED TO MAKE STORE_INFO AND STORE_PRODUCTS TABLES -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop all your electronic needs and save big! | ABDGameStore.com</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <?php include_once "navbar.php"; ?>
+
+    <div class="wrapper">
+        <p><a href="?controller=home&action=index">Home</a> / All Sellers <b>
+        <div class="container categories">
+            <div class="product-categories">
+                <h1>Sellers</h1>
+                <a href="?controller=product&action=categories&page=1">All</a>
+                <?php
+                foreach($categories as $c) {
+                    $lower = strtolower($c -> Prod_Category);
+                    echo "
+                    <a href='?controller=product&action=categories&category={$lower}&page=1'>
+                    {$c -> Prod_Category}
+                    </a>
+                    ";
+                }
+                ?>
+            </div>
+            <div class="product-boxes-categories">
+            <ol class="ol-product-case">
+                <?php
+                foreach($category as $product) {
+                    echo 
+                    '<li class="li-product-item">
+                        <div class="product-inside-list">
+                            <a href="?controller=product&action=view&id='. $product -> Prod_ID .'">
+                                <img src="'. $product -> Prod_Image_Path .'">
+                            </a>
+                            <div class="li-product-bottom">
+                                <p id="list-product-name">'. $product -> Prod_Name .'</p>
+                                <p id="list-product-price">'. $product -> Prod_Client_Price .'$</p>
+                            </div>
+                        </div>
+                    </li>
+                    ';
+                }
+                ?>
+            </ol>
+            </div>
+        </div>
+        <div class="pagination">
+                <p>Page <?php echo $page + 1 ?> of <?php echo $pages ?></p>
+                <a href="?controller=product&action=categories&page=1">First</a>
+                <?php if(isset($_GET['page']) && $_GET['page'] > 1) {
+                    $previous = $_GET['page'] - 1;
+                    echo "<a href='?controller=product&action=categories&page={$previous}'>Previous</a>";
+                } else {
+                    echo "Previous";
+                } ?>
+                <div class="page-numbers">
+                    <?php
+                    for ($i = 1; $i <= $pages; $i++) {
+                        echo "<a href='?controller=product&action=categories&page={$i}'>$i</a>";
+                    }
+                    ?>
+                </div>
+                <?php
+
+                if (!isset($_GET['page'])) {
+                    ?>
+                    <a href="?controller=product&action=categories&page=2">Next</a>
+                    <?php
+                } else {
+                    if ($_GET['page'] >= $pages) {
+                        ?>
+                        <a>Next</a>
+                        <?php
+                    } else {
+                        $next = $_GET['page'] + 1;
+                        echo "<a href='?controller=product&action=categories&page={$next}'>Next</a>";
+                    }
+                }
+
+                ?>
+                <a href="?controller=product&action=categories&page=<?php echo $pages ?>">Last</a>
+            </div>
+        <div class="push"></div>
+    </div>
+
+    <?php include_once "footer.php"; ?>
+
+<script src="app.js"></script>
+
+</body>
+</html>
